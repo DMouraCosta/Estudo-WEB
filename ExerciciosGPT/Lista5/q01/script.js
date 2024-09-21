de0b72180f2724b4851a70768cc4db1f66097bbe
DMouraCosta <dailson100@gmail.com>
2024-09-21 18:41:39 -0300

    Finalizando a questão 1 da lista 05

---
+++ ExerciciosGPT/Lista5/q01/script.js
@@ -0,0 +1,7 @@
+let bt = document.getElementById('mais');
+
+bt.addEventListener('click',
+    function saibaMais(){
+        alert("Este é o Melhor fone do mundo, pode comprar sem medo!")
+    }
+);
